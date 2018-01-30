873d70b9d99043fff6908662b06318e44aa02993
Brandon Rhodes <brandon@rhodesmill.org>
2018-01-29 22:40:22 -0500

    Get old test working again

--- gang-of-four/decorator-pattern/tests.py
+++ gang-of-four/decorator-pattern/tests.py
@@ -1,17 +1,10 @@
 import logging
-import os
-import sys
 import tempfile
 import unittest
 
-# Allow modules to be imported from the current directory.
-sys.path.insert(0, os.path.dirname(__file__))
-
-import copy_powered_wrapper
-import getattr_powered_wrapper
-import verbose_static_wrapper
-
-import test.test_file
+#from . import copy_powered_wrapper
+from . import getattr_powered_wrapper
+from . import verbose_static_wrapper
 
 def wrap(cls, normal_file):
     logger = logging.getLogger('testlog')
@@ -25,51 +18,15 @@ class BaseCase(unittest.TestCase):
         logger = logging.getLogger('testlog')
         return self.class_under_test(normal_file, logger)
 
-class AutoTests1(test.test_file.AutoFileTests, BaseCase):
-    class_under_test = verbose_static_wrapper.WriteLoggingFile
-
-    @unittest.skip('TODO: why does this fail?')
-    def testReadinto_text(self):
-        raise unittest.Skip()
-
-class OtherTests1(test.test_file.OtherFileTests, BaseCase):
-    class_under_test = verbose_static_wrapper.WriteLoggingFile
-
-class AutoTests2(test.test_file.AutoFileTests, BaseCase):
-    class_under_test = getattr_powered_wrapper.WriteLoggingFile
-
-    @unittest.skip('TODO: why does this fail?')
-    def testReadinto_text(self):
-        raise unittest.Skip()
-
-class OtherTests2(test.test_file.OtherFileTests, BaseCase):
-    class_under_test = getattr_powered_wrapper.WriteLoggingFile
-
-# class AutoTests3(test.test_file.AutoFileTests, BaseCase):
-#     def open(self, *args, **kw):
-#         normal_file = open(*args, **kw)
-#         return copy_powered_wrapper.WriteLoggingFile(normal_file)
-
-
-#     @unittest.skip('TODO: why does this fail?')
-#     def testReadinto_text(self):
-#         raise unittest.Skip()
-
-# class OtherTests3(test.test_file.OtherFileTests, BaseCase):
-
-#     # @unittest.skip('TODO: make this insensitive to case')
-#     # def testIteration(self):
-#     #     raise unittest.Skip()
-
 class MyTests(BaseCase):
     def test_some_attribute_behaviors(self):
-        for module in (
-                verbose_static_wrapper,
-                getattr_powered_wrapper,
+        for cls in (
+                verbose_static_wrapper.WriteLoggingFile1,
+                getattr_powered_wrapper.WriteLoggingFile3,
         ):
             with tempfile.TemporaryFile('w+') as f:
                 logger = logging.getLogger('testlog')
-                w = module.WriteLoggingFile(f, logger)
+                w = cls(f, logger)
                 self.assertEqual(f.newlines, w.newlines)
                 with self.assertRaisesRegex(AttributeError, '.*TextIO.*'):
                     # Make sure at least one access is really going back
