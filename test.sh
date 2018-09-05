836c24a273e072f645556fabe0e8f1bf5b088ce6
Brandon Rhodes <brandon@rhodesmill.org>
2018-09-05 17:51:27 -0400

    Make test script also run doctests

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@
 # Run this with Ubuntu Python 3.6, since unlike Anaconda Python it seems
 # to come with the Python Standard Library test suite built-in.
 
-exec python3.6 -m unittest "$@" gang-of-four/*/test*.py
+python3.6 -m unittest "$@" gang-of-four/*/test*.py && make doctest
