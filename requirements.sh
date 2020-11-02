7c236796bc0cfab9c3aee0d601c6d862f030475c
Brandon Rhodes <brandon@rhodesmill.org>
2020-11-02 15:47:10 -0500

    Return to having a normal “requirements.txt” file

--- requirements.sh
+++
@@ -1,2 +0,0 @@
-# Start with a conda env with "python=3.6"
-conda install python=3.6 sphinx=1.8.2 matplotlib=2.2.2
--- test.sh
+++ test.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-# Run this with Ubuntu Python 3.6, since unlike Anaconda Python it seems
-# to come with the Python Standard Library test suite built-in.
-
-/usr/bin/python3 -m unittest "$@" gang-of-four/*/test*.py && make doctest
+python3.6 -m unittest "$@" gang-of-four/*/test*.py && make doctest
